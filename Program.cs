97ea6b769093042c8a0c5973c00dbe8758ce7b77
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-04 15:59:01 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividad_10__09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cree un diccionario de notas. Solicite un nombre por teclado y verifique si existe antes de mostrar la nota.");
+        
+        }
+    }
+}
