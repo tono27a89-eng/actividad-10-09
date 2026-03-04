1bdb795011181542a8e77360837cc5ec92d9d2f3
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-04 16:04:07 -0600

    recopilacion

--- Program.cs
+++ Program.cs
@@ -11,7 +11,53 @@ namespace actividad_10__09
         static void Main(string[] args)
         {
             Console.WriteLine("Cree un diccionario de notas. Solicite un nombre por teclado y verifique si existe antes de mostrar la nota.");
-        
+           Dictionary<string, double> notas = new Dictionary<string, double>();
+            string nombre;
+            double notas1;
+            Console.WriteLine("ingrese cantidad de notas que desea ingresar: ");
+            int.TryParse(Console.ReadLine(), out int lim);
+            for (int i = 0; i < lim; i++)
+            {
+                Console.WriteLine($"Ingrese  nombre NO.{i + 1} de {lim}");
+                nombre= Console.ReadLine();
+                if (estudiante.ContainsKey(nombre))
+                {
+                    Console.WriteLine("nombre ya ingresado");
+                    i--;
+
+                }
+                else
+                {
+                    Console.WriteLine("ingrese nombre:");
+                    nombre = Console.ReadLine();
+                    estudiante.Add(Id, nombre);
+
+                }
+
+            }
+            bool validar = false;
+            while (validar != true)
+            {
+
+
+                Console.WriteLine("ingrese ID de estudiante que desea elimar");
+                int.TryParse(Console.ReadLine(), out Id);
+                if (estudiante.ContainsKey(Id))
+                {
+                    estudiante.Remove(Id);
+                    validar = true;
+                }
+                else
+                {
+                    Console.WriteLine("ID invalido");
+                }
+            }
+            foreach (var item in estudiante)
+            {
+                Console.WriteLine("prodcuto Id: " + item.Key + "valor: " + item.Value);
+
+            }
+
         }
     }
 }
